7b7fde2146ed03081fb2cfbb8d070776f2f3012f
Filippo Fontana <fontanaf@ethz.ch>
2018-04-12 09:16:20 +0200

    Requests

--- GuestWiFi.py
+++ GuestWiFi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from functions import generate_pwd, ssh_pwd_change
 from flask import Flask, request, render_template
+from escpos.printer import Network
 
 pwdLength = 10
 
@@ -17,5 +18,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    if request.method = 'POST':
+        pwd = generate_pwd(pwdLength)
     content = {'wifiPwd': 'ciao!'}
     return render_template('main.html', **content)
